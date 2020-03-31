d78ca6478790fb68d44836abc2de886d6709c16c
chia <chiahui@senzing.com>
2020-03-31 18:16:23 +0800

    fixed s3 output

--- services/s3_state/outputs.tf
+++ services/s3_state/outputs.tf
@@ -3,7 +3,7 @@ output "S3_arn" {
   description = "The arn of the s3 bucket"
 }
 
-output "dynamodb_id" {
+output "S3_id" {
   value       = aws_s3_bucket.s3_bucket.id
   description = "The name of the s3 bucket"
 }
